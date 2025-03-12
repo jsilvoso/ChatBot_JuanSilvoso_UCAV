2551c7314c6e2ba319756d9f58149dd45b8c1e5c
jsilvoso <jsilvoso@hotmail.com>
2025-03-12 10:15:26 +0100

    Actualización Chatbot

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def chat_transformers_response():
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Render asigna
+    app.run(host="0.0.0.0", port=port)
